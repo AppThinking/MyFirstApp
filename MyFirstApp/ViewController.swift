0a3efbf133b472d0536495f845fa197c7e238ec7
Stefan Cahill <Stefan@stefans-air.fritz.box>
2014-12-12 15:53:23 +0100

    erstes Review

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nachrichtLabel: UILabel!
+    @IBOutlet weak var nameEingebenTextFeld: UITextField!
+    @IBOutlet weak var nachrichtEingebenTextFeld: UITextField!
+    @IBOutlet weak var nachrichtSendenButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nachrichtSendenButtonGedrueckt(sender: UIButton) {
+    
+        nachrichtLabel.text = nachrichtEingebenTextFeld.text
+        nachrichtLabel.hidden = false
+        nachrichtLabel.textColor = UIColor.redColor()
+        
+        nachrichtEingebenTextFeld.text = ""
+        
+        // Ausblenden des KeyBoards nach drücken des Senden Buttons
+        nachrichtEingebenTextFeld.resignFirstResponder()
+        
+        nachrichtSendenButton.setTitle("Nachricht gesendet", forState: UIControlState.Normal)
+        
+    }
 
 }
 
